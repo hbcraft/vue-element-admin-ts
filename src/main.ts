a47d734d674b05d7bf87518c26e3f1ffc8b25bb6
jyh <admin@hbcraft.cn>
2020-12-25 10:25:14 +0800

    refactor(i18n): 移动i18n.ts到plugins目录下

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import store from './store'
 
 import './plugins/element'
 import './plugins/axios'
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
